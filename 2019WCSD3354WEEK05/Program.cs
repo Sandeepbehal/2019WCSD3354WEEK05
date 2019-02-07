655b4b9cf6ff2f32b9494d1f93be88de96869d8e
visualstudio <visualstudio@230-PC44>
2019-02-07 15:52:36 -0500

    commit at 3:52

--- 2019WCSD3354WEEK05/Program.cs
+++ 2019WCSD3354WEEK05/Program.cs
@@ -36,3 +36,74 @@ namespace _2019WCSD3354WEEK05
             return true;
         }
     }
+
+    class dog
+    {
+        public dog(string name, string breed)
+        {
+            dog_name = name;
+            dog_breed = breed;
+        }
+
+
+
+        public string dog_name;
+        public string dog_breed;
+        public dog next_dog;
+        public dog prev_dog;
+
+    }
+    class birthday_party
+    {
+        public dog peanut;
+        public dog fifi;
+        public dog clarence;
+        public dog roy;
+
+        public dog head;
+        public dog tail;
+        public dog temporary;
+
+
+
+        public void mapini()
+        {
+            peanut = new dog("peanut", "bichon");
+            fifi = new dog("fifi", "poodle");
+            clarence = new dog("clarence", "german");
+            roy = new dog("roy", "beagle");
+
+
+            peanut.prev_dog = null;
+            peanut.next_dog = fifi;
+
+            fifi.prev_dog = peanut;
+            fifi.next_dog = clarence;
+
+            clarence.prev_dog = fifi;
+            clarence.next_dog = roy;
+
+            roy.prev_dog = clarence;
+            roy.next_dog = null;
+            head = peanut;
+            tail = roy;
+
+
+
+        }
+        public void run()
+        {
+            this.mapini();
+            Console.WriteLine(dognames);
+        }
+
+        public void dognames()
+        {
+            head = peanut;
+            while (temporary.next_dog != null)
+            {
+                Console.WriteLine(temporary.dog_name);
+            }
+        }
+    }
+}
